6c13bf7defb53abfc942da7feab34a8be45c249c
sjsakib <sjsakib.bd@gmail.com>
2018-12-25 02:21:36 +0600

    using https for api

--- src/lib/wb.js
+++ src/lib/wb.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const wb = axios.create({
-	baseURL: 'http://api.worldbank.org/v2/countries/BD/indicators/',
+	baseURL: 'https://api.worldbank.org/v2/countries/BD/indicators/',
 	params: { date: '1990:2018', format: 'json' }
 });
 
