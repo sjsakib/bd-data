29fb0d46fcc71124ea36582e5b026a2747ac5e73
sjsakib <sjsakib.bd@gmail.com>
2018-12-25 10:12:38 +0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect
+} from 'react-router-dom';
 import Chart from './Chart';
 import indicators from './indicators';
 
 class App extends Component {
-  state = { currentIndicator: 0 };
   render() {
-    const current = this.state.currentIndicator;
-    const indicator = indicators[current];
     return (
       <div className="App ui grid stackable">
-        <div className="ui row">
-          <div className="twelve wide column">
-            <Chart
-              {...indicator}
-            />
-          </div>
-          <div className="four wide column">
-            <h1>Indicators</h1>
-            <div className="ui middle aligned selection list">
-              {indicators.map((indicator, i) => (
-                <div
-                  className={`item ${i === current ? 'active' : ''}`}
-                  key={indicator.id}
-                  onClick={() => this.setState({ currentIndicator: i })}>
-                  <div className="content">
-                    <div className="header">{indicator.name}</div>
-                    <div className="description">{indicator.info}</div>
-                  </div>
-                </div>
+        <Router>
+          <div className="ui row">
+            <div className="twelve wide column">
+              <Route
+                exact
+                path="/"
+                render={() => <Redirect to={'/' + indicators[0].id} />}
+              />
+              {indicators.map(indicator => (
+                <Route
+                  path={'/' + indicator.id}
+                  exact
+                  render={() => <Chart {...indicator} />}
+                />
               ))}
             </div>
+            <div className="four wide column">
+              <h1>Indicators</h1>
+              <div className="ui middle aligned selection list">
+                {indicators.map((indicator, i) => (
+                  <NavLink
+                    to={'/' + indicator.id}
+                    className="item"
+                    activeClassName="active"
+                    key={indicator.id}>
+                    <div className="content">
+                      <div className="header">{indicator.name}</div>
+                      <div className="description">{indicator.info}</div>
+                    </div>
+                  </NavLink>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
+        </Router>
       </div>
     );
   }
